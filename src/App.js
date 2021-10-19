ff869b556ab6f8d2420065a5289ba40b087355eb
sadafamininia99 <aminicomputer96@gmail.com>
2021-10-19 20:35:42 +0330

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+// import React from 'react';
+import Header from './Components/Header';
+
+function App() {
+ return(
+<div className="container">
+   < Header/>
+</div>
+ );
+
+}
+//class componemt
+
+// class App extends React.Component{
+//   render() {
+//     return <h1>
+//       hi im sadaf
+//     </h1>
+//   },
+// }
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>
+
+            Task Tracker
+             </h1>
+        </header>
+    ) 
+}
+
+export default Header
