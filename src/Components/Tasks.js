461828331afce8ccad204754b8d2d570a5d3d3df
sadafamininia99 <aminicomputer96@gmail.com>
2021-10-20 16:04:02 +0330

    add task component ad a array consist of some object

---
+++ src/Components/Tasks.js
@@ -0,0 +1,33 @@
+//  import React from 'react'
+const task =[
+    {
+    id : 1,
+    tetxt: 'doctore appointment',
+    day : 'Feb 5th at 2:40pm',
+    reminder : true,
+  
+    },
+    {
+        id :2,
+        tetx :'meeting js',
+        day :'Oct 8th at 12:00am',
+        reminder :true,
+    },
+    {
+        id :3,
+        text :'food shopping',
+        day :'Oct 22 at 13:10pm '
+        reminder :false
+    }
+
+]
+   
+ 
+ export const Tasks = () => {
+     return (
+         <div>
+             
+         </div>
+     )
+ }
+ 
