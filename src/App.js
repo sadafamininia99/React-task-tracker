a54605976b3c08764662ee2ebf9b4b7e7828b845
sadafamininia99 <aminicomputer96@gmail.com>
2021-10-21 16:12:47 +0330

    I ADD Tasks componet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
+
 // import React from 'react';
 import Header from './Components/Header';
+import Tasks   from './Components/Tasks';
 
 function App() {
  return(
 <div className="container">
    < Header/>
+   <Tasks/>
 </div>
  )
 
