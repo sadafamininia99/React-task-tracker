3e6bbe23a2ce9e458a9c86414442591570e20245
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-10-21 17:14:40 +0330

    [format] update code formatting

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import './App.css';
 
 // import React from 'react';
 import Header from './Components/Header';
-import Tasks   from './Components/Tasks';
+import Tasks from './Components/Tasks';
 
 function App() {
- return(
-<div className="container">
-   < Header/>
-   <Tasks/>
-</div>
- )
-
+  return (
+    <div className='container'>
+      <Header />
+      <Tasks />
+    </div>
+  );
 }
 //class componemt
 
--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,38 +1,34 @@
 //  import React from 'react'
-const tasks =[
-    {
-    id : 1,
+const tasks = [
+  {
+    id: 1,
     text: 'doctore appointment',
-    day : 'Feb 5th at 2:40pm',
-    reminder : true,
-  
-    },
-    {
-        id :2,
-        text :'meeting js',
-        day :'Oct 8th at 12:00am',
-        reminder :true,
-    },
+    day: 'Feb 5th at 2:40pm',
+    reminder: true
+  },
+  {
+    id: 2,
+    text: 'meeting js',
+    day: 'Oct 8th at 12:00am',
+    reminder: true
+  },
 
-    {
-        id :3,
-        text :'food shopping',
-        day :'Oct 22 at 13:10pm ',
-        reminder :false,
-    }
+  {
+    id: 3,
+    text: 'food shopping',
+    day: 'Oct 22 at 13:10pm ',
+    reminder: false
+  }
+];
 
-]
-   
+const Tasks = () => {
+  return (
+    <>
+      {tasks.map(task => (
+        <h1>{task.text}</h1>
+      ))}
+    </>
+  );
+};
 
-   const Tasks = () => {
-     return (
-
- <>
- {tasks.map((task) => (<h1>{task.text}</h1>) 
-     )}
- 
-        </>
-     )
- }
- 
- export default Tasks
+export default Tasks;
