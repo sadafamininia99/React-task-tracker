7446af4c455d2a6209868e6ef419dd19a779804e
sadafamininia99 <aminicomputer96@gmail.com>
2021-12-04 22:01:43 +0330

    init

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-// import logo from './logo.svg';
-import './App.css';
-
-// import React from 'react';
-import Header from './Components/Header';
-import Tasks   from './Components/Tasks';
 
 function App() {
  return(
 <div className="container">
-   < Header/>
-   <Tasks/>
+  <Todolist/>
 </div>
  )
 
 }
-//class componemt
-
-// class App extends React.Component{
-//   render() {
-//     return <h1>
-//       hi im sadaf
-//     </h1>
-//   },
-// }
-
 export default App;
